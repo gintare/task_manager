937b49f811ace78d750707f6539e391ca5d59035
gintare <gintare.ragaisiene@gmail.com>
2022-04-30 15:33:09 +0300

    Added plain starter message on root http:://localhost:8080 link.

--- src/main/java/com/gintare/tasksmanager/TasksmanagerApplication.java
+++ src/main/java/com/gintare/tasksmanager/TasksmanagerApplication.java
@@ -2,12 +2,20 @@ package com.gintare.tasksmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TasksmanagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksmanagerApplication.class, args);
+	}
+
+	@GetMapping
+	public String starterMessage(){
+		return "Get ready! Task Manager coming soon!";
 	}
 
 }
