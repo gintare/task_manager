d3310e98ac5249dba6fc8bf98a9bb7621d46b380
gintare <gintare.ragaisiene@gmail.com>
2022-04-30 17:03:09 +0300

    Added Swagger, localhost:8080/swagger-ui.html documentation.

--- src/main/java/com/gintare/tasksmanager/TasksmanagerApplication.java
+++ src/main/java/com/gintare/tasksmanager/TasksmanagerApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@EnableSwagger2
 public class TasksmanagerApplication {
 
 	public static void main(String[] args) {
